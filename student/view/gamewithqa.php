ed4c251147daa0c3c14b78dd4b27e14169f66311
nathsu0 <nathverbo00@gmail.com>
2021-07-15 19:31:37 +0800

    Update gamewithqa.php

--- student/view/gamewithqa.php
+++ student/view/gamewithqa.php
@@ -18,14 +18,17 @@
 <script>
   function radioget(getValue){
     var awit = getValue;
+    console.log(awit);
      $.ajax({
                     type: "POST",
                     url: 'upload.php',
                     data: { awit : awit},
-                    success: function(data){
+                    success: function(result)
+                    {
+                        
                     }
                 });
-    console.log(awit);
+    
   }
 
 </script>
@@ -41,32 +44,115 @@
             <textarea id = "question<?php echo $count;?>" disabled><?php echo $count;?>. <?php echo $row['Question']; $_SESSION['Question'.$count]=$row['Question']; ?></textarea>
         </div>
        
-   <form method="POST">
+   
         <div class="choice">
-          <input type="radio" id="answer1<?php echo $count;?>" name="choices<?php echo $count; ?>" value="D" onchange="radioget($(this).val())">
-          <label for="answer1<?php echo $count;?>">A. <?php echo $row['A']; $_SESSION['A'.$count]=$row['A'];?></label>
+          <input type="radio" id="answer1<?php echo $count;?>" name="choices<?php echo $count; $_SESSION['A'.$count]=$row['A'];?>" value="A" onchange="radioget($(this).val())">
+          <label for="answer1<?php echo $count;?>">A. <?php echo $row['A'];?></label>
         </div>
           <div class="choice">
-          <input type="radio" id="answer2<?php echo $count;?>" name="choices<?php echo $count; ?>" value="C" onchange="radioget($(this).val())">
-          <label for="answer2<?php echo $count;?>">B. <?php echo $row['B']; $_SESSION['B'.$count]=$row['B'];?></label>
+          <input type="radio" id="answer2<?php echo $count;?>" name="choices<?php echo $count; $_SESSION['B'.$count]=$row['B'];?>" value="B" onchange="radioget($(this).val())">
+          <label for="answer2<?php echo $count;?>">B. <?php echo $row['B'];?></label>
         </div>
           <div class="choice">
-          <input type="radio" id="answer3<?php echo $count;?>" name="choices<?php echo $count;?>" value="B" onchange="radioget($(this).val())">
-          <label for="answer3<?php echo $count;?>">C. <?php echo $row['C'];  $_SESSION['C'.$count]=$row['C'];?></label>
+          <input type="radio" id="answer3<?php echo $count;?>" name="choices<?php echo $count; $_SESSION['C'.$count]=$row['C'];?>" value="C" onchange="radioget($(this).val())">
+          <label for="answer3<?php echo $count;?>">C. <?php echo $row['C'];?></label>
           </div>
           <div class="choice">
-          <input type="radio" id="answer4<?php echo $count;?>" name="choices<?php echo $count;?>" value="A" onchange="radioget($(this).val())">
-          <label for="answer4<?php echo $count;?>">D. <?php echo $row['D']; $_SESSION['D'.$count]=$row['D'];?></label>
+          <input type="radio" id="answer4<?php echo $count;?>" name="choices<?php echo $count; $_SESSION['D'.$count]=$row['D'];?>" value="D" onchange="radioget($(this).val())">
+          <label for="answer4<?php echo $count;?>">D. <?php echo $row['D'];?></label>
         </div>
-      
-    </form>
+
           <div class="choice-button">
           <button type="button" id="submitButton<?php echo $count;?>" class="button btn-bubble<?php echo $count;?>">Submit</button>
         </div>
       </div>
 </div>
 
+<div id="instructionModal" class="modal3" style="display: none;">
+  <div class="modal-contentw">
+    <span class="close">
+      <ul >
+        <li>The players will have five (5) life points every game.</li>
+        <li>The players should pop the bubbles orderly to move to the next level.</li>
+        <li>Every wrong pop of bubbles, the life points will decrease by one (1). </li>
+        <li>The players must have at least one (1) life points to finish all levels.</li>
+        <li>The players should answer correctly to gain points.</li>
+        <li>Questions and answers are in multiple choice format. The players need to choose only one (1) answer.</li>
+        <li>The timer is set for only seven(7) seconds in every level to show the numbers inside the bubble.</li>
+        <li>For every level completed, the player will gain 10 points.</li>
+        <li>The total scores will show after the player completes the game or if all life points are consumed.</li>
+      </ul>  
+      </div>
+  </div>
+</div>
+ <!------------------------------------------->
+<!-- POP UP -->
 
+<div id="popup1" class="overlay">
+  <div class="popup-correct" id="popup-correct">
+      <h2 style='color:green;'>Correct &#10004;</h2>
+      <a class="close" href="#">×</a>
+      <div class="content">
+        <strong>Your response : </strong><labe>Penoy</label><br></br>
+        <strong>Answer : </strong>Penoy
+      </div>
+    </div>
+    <div class="popup-wrong "id="popup-wrong">
+      <h2 style='color:red;'>Wrong &#10006;</h2>
+      <a class="close" href="#">×</a>
+      <div class="content">
+        <strong>Your response : </strong><labe>balot</label><br></br>
+        <strong>Answer : </strong>Penoy
+      </div>
+    </div>
+  </div>
+
+  <!------------------------------------------->
+
+
+<!-- <div id="evaluationModal" class="modal2" style="display: none;">
+
+  <div class="modal-content">
+    <span class="close" style="font-size:60px; margin-right: -20%; margin-top: 12%;">&times;</span>
+    <div><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <table>
+        <tr>
+          <th>Questions</th>
+          <th>Your Answers</th>
+          <th>Correct Answers</th>
+        </tr>
+        <tr>
+          <td>1. Who is our National Hero</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>2. Who is our National Hero</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>3. Who is our National Hero</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>4. Who is our National Hero</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>5. Who is our National Hero</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+      </table>
+        <button id="restartButton" >Restart Game</button>
+        <button id="leaderboardsButton" >Leaderboards</button>
+        <button id="exitButton" >Exit Game</button>
+      </div>
+  </div>
+</div> -->
 
 <div id="gameOverModal" class="modal4" >
   <div class="modal-content">
@@ -79,28 +165,13 @@
 </div> 
 
 <?php
-$feed = TRUE;
-if($_SESSION['answer']==$row['Answer']){
-$feed = TRUE;
-echo '<script type="text/javascript">' .
-'console.log("tama naman");</script>';
-}else{
-  $feed = FALSE;
-  echo '<script type="text/javascript">' .
-'console.log("mali");</script>';
-}
-?>
-
-<?php
 $count++;
     }
 ?>
 
     <canvas id="canvas1"></canvas>
-    <script type="text/javascript">var qcount = "<?= $qcount?>";var awit = "<?= $feed?>"</script>
+    <script type="text/javascript">var qcount = "<?= $qcount?>"</script>
     <script type="text/javascript" src="../js/gamewithquestion.js"></script>
-
-
  
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.9/jquery-ui.js" type="text/javascript"></script>
