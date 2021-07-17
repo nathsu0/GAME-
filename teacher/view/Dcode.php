889589c56cc5ff31cb799a3a2f7633faa363c46b
nathsu0 <nathverbo00@gmail.com>
2021-07-17 13:50:34 +0800

    Updated Delete

--- teacher/view/Dcode.php
+++ teacher/view/Dcode.php
@@ -13,6 +13,8 @@
         $conn->query( "INSERT INTO $code.$TABLE SELECT * FROM $decode.$TABLE" ) or die( $conn->error );
 
     endwhile;
+    $newconn = new mysqli ('localhost', 'root' , '',$code);
+    $del = mysqli_query($newconn, "DELETE FROM scores");
     include 'questiondb.php';
     $ques= mysqli_connect ('localhost', 'root' , '', 'question');  
     $result6 = mysqli_query($ques,"SELECT * FROM tanong WHERE CODE='$decode'");
--- teacher/view/Delete.php
+++ teacher/view/Delete.php
@@ -5,7 +5,6 @@
     $result = mysqli_query($conn, "DROP DATABASE $code");
     $new = new mysqli ('localhost', 'root', '', 'question');
     $sql = mysqli_query($new, "DELETE FROM tanong WHERE CODE = '$code'"); 
-    echo"<script>alert('Game Deleted!')</script>";
     echo '<script type="text/javascript">' .'window.location = "EditView.php"' . '</script>';
 
 ?>
