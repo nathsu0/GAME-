19d7d1092ecc0f7d3fc4e0d6d6c231f6a7d01f73
nathsu0 <nathverbo00@gmail.com>
2021-08-16 21:31:11 +0800

    ...

--- teacher/view/Delete.php
+++ teacher/view/Delete.php
@@ -2,9 +2,14 @@
     include "conn.php";
     session_start();
     $code = $_GET['code'];
-    
+    $aw = mysqli_query($conn,"SELECT * from quiz WHERE gamecode='$code'");
+
     $sql = mysqli_query($conn, "DELETE FROM  quiz WHERE  gamecode='$code'"); 
     $del = mysqli_query($conn, "DELETE FROM  quiz_question WHERE  gamecode='$code'"); 
+    $row = mysqli_fetch_assoc($aw);
+    $tw = $row['ID'];
+    $nye = mysqli_query($conn,"DELETE FROM quiz_attemp where quiz_id = '$tw'");
+
     echo '<script type="text/javascript">' .'window.location = "EditView.php"' . '</script>';
 
 ?>
--- teacher/view/QuestionTable.php
+++ teacher/view/QuestionTable.php
@@ -13,7 +13,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title><?=$row6['GAMENAME'];?></title>
+    <title><?=$row6['gamename'];?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
